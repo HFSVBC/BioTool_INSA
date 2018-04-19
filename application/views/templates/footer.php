cbb1126d5fa6100bf13ebf8034f9fe81d8c97f91
Hugo Curado <hfsvbc@hugocurado.info>
2018-04-19 09:15:56 +0100

    Tamplate started

---
+++ application/views/templates/footer.php
@@ -0,0 +1,5 @@
+  <footer class="fixed-bottom bg-dark text-light py-2 px-3">
+    <p>© <?php echo Date('Y');?>, BioPercentis</p>
+  </footer>
+</body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,54 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+  <title>BioPercentis</title>
+  <meta name="description" content="">
+  <link rel="canonical" href="">
+  <!-- Facebook Open Graph -->
+  <meta property="og:title" content="">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+  <meta property="og:site_name" content="">
+  <meta property="og:description" content="">
+  <!-- Twitter Cards -->
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:title" content="">
+  <meta name="twitter:description" content="">
+  <meta name="twitter:image" content="">
+  <!-- Website Icon -->
+  <!-- <link rel="shortcut icon" href="" type="image/x-icon"> -->
+  <!-- jQuery JS -->
+  <script type="text/javascript" src="<?php echo base_url('libraries/jQuery/jquery-3.2.1.min.js');?>"></script>
+  <!-- Popper JS -->
+  <script type="text/javascript" src="<?php echo base_url('libraries/popper/popper.js');?>"></script>
+  <!-- Bootstrap -->
+  <link async rel="stylesheet" type="text/css" href="<?php echo base_url('frameworks/bootstrap/css/bootstrap.min.css');?>">
+  <script async type="text/javascript" src="<?php echo base_url('frameworks/bootstrap/js/bootstrap.min.js');?>"></script>
+  <!-- Main CSS -->
+  <link async rel="stylesheet" type="text/css" href="<?php echo base_url('custom/css/main.css');?>">
+</head>
+<body>
+  <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">BioPercentis</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://github.com/HFSVBC/BioPercentis_INSA" target="_blank">Source</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
